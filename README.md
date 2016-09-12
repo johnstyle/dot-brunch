2876d1a4890ecdec438613f19073008c6898224c
Jonathan SAHM <contact@johnstyle.fr>
2016-09-12 23:38:25 +0200

    Update plugin

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dot-brunch
+
+Beta version
+
+
--- index.js
+++ index.js
@@ -2,24 +2,26 @@
 
 const doT = require('dot');
 
+let first;
+
 class doTCompiler {
   constructor(config) {
-
-    this.config = config && config.plugins && config.plugins.doT;
+    this.config = config && config.plugins && config.plugins.dot;
+    if ('function' !== typeof this.config.pathName) {
+      this.config.pathName = function (path) {
+        return path;
+      }
+    }
+    first = true;
   }
   compile(params) {
-    console.log('ok');
     const data = params.data;
     const path = params.path;
-
-    const options = clone(this.options);
-    options.filename = path;
-
     return new Promise((resolve, reject) => {
           let result, error;
     try {
-      let compiled = doT.process({ path: "./views"});
-      result = umd(compiled);
+      result = (true === first ? "var JST = {};" : "") + "JST['" + this.config.pathName(path) + "'] = " + doT.template(data.toString('utf8')).toString() + ";";
+      first = false;
     } catch (_error) {
       error = _error;
     } finally {
